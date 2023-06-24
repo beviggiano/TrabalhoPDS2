cb1a96411f1075b7ec80031a413c980566bf579d
Bernardo Viggiano <126487673+beviggiano@users.noreply.github.com>
2023-06-24 14:52:35 -0300

    Partição do método que faz a leitura doc por doc.

--- src/motorbusca.cpp
+++ src/motorbusca.cpp
@@ -5,3 +5,25 @@ MotorBusca::MotorBusca(const std::vector<std::string>& nomesArquivos) {
         documentos.push_back(Documento(nome));
 }
 
+void MotorBusca::buscar(std::string& consulta){
+    std::vector<std::string> palavrasConsulta;
+    std::istringstream fluxo(consulta);
+    std::string palavra;
+
+    while (fluxo >> palavra) {
+        Documento::normalizar(palavra); // Use Documento::normalizar instead of documentos[0].normalizar
+        palavrasConsulta.push_back(palavra);
+    }
+
+    std::vector<std::pair<std::string, int>> resultados;
+
+    for (auto& doc : documentos) {
+        if (todasPalavrasExistem(doc, palavrasConsulta)) {
+            int frequenciaTotal = 0;
+            for (const auto& palavra : palavrasConsulta)
+                frequenciaTotal += doc.obterFrequenciaPalavra(palavra);
+            resultados.push_back(std::make_pair(doc.obterNomeArquivo(), frequenciaTotal));
+        }
+    }
+    
+}
