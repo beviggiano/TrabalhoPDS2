6e819f978e056c7f6b5bfebe39c060fc37337616
GustavoVHG <gustavovhg1@gmail.com>
2023-06-24 14:42:43 -0300

    Criando"carregarDocumento" e "obterNomeArquivo"

--- src/documento.cpp
+++ src/documento.cpp
@@ -9,3 +9,17 @@ void Documento::normalizar(std::string& palavra) {
     palavra.erase(std::remove_if(palavra.begin(), palavra.end(), [](char c) { return !isalpha(c); }), palavra.end());
     std::transform(palavra.begin(), palavra.end(), palavra.begin(), ::tolower);
 }
+
+void Documento::carregarDocumento() {
+    std::ifstream arquivo(nomeArquivo);
+    std::string palavra;
+
+    while (arquivo >> palavra) {
+        normalizar(palavra);
+        mapaPalavras[palavra]++;
+    }
+}
+
+const std::string& Documento::obterNomeArquivo() {
+    return nomeArquivo;
+}
